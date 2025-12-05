aa6988143adf293f51f1ab58055858ac732a7010
nikita zigman <zigman.nikita@gmail.com>
2025-12-05 23:55:34 +0000

    Add utils to the routers

---
+++ internal/router/utils.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func jsonResponse(w http.ResponseWriter, status int, data any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(data)
+}
+
+func jsonError(w http.ResponseWriter, status int, message string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(ErrorResponse{Error: message})
+}
